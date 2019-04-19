b3486a3477fcf760e114233f873bcf195ed3d80b
Arthur Prather <apratherjr@gmail.com>
2019-04-18 22:41:46 -0500

    fix eslint issue with anchor link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
+// eslint-disable-line no-use-before-define
 const Contact = () => (
-  <Fragment>
-    <h1>This is Contact Page</h1>
-    <div className="info">
-      <p>
-        <strong>Email:</strong> <a href="#">Reachme@email.com</a>
-      </p>
-      <p>
-        <strong>Phone:</strong> 899-123-456
-      </p>
-    </div>
-  </Fragment>
+	<Fragment>
+		<h1>This is Contact Page</h1>
+		<div className="info">
+			<p>
+				<strong>Email:</strong>
+
+				<a href="_blank">Reachme@email.com</a>
+			</p>
+			<p>
+				<strong>Phone:</strong> 899-123-456
+			</p>
+		</div>
+	</Fragment>
 );
 
 export default Contact;
